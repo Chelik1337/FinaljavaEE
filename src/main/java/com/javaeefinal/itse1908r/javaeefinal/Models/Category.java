2c3ca08a3296da3eb0bfbfe846564e1c438dbfe0
Alibek <ali.mazhikenov@gmail.com>
2021-12-25 01:09:40 +0600

    New entity + persistence.xml

---
+++ src/main/java/com/javaeefinal/itse1908r/javaeefinal/Models/Category.java
@@ -0,0 +1,31 @@
+package com.javaeefinal.itse1908r.javaeefinal.Models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/javaeefinal/itse1908r/javaeefinal/Models/Institution.java
@@ -0,0 +1,65 @@
+package com.javaeefinal.itse1908r.javaeefinal.Models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "institutions")
+public class Institution {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "address", nullable = false, length = 50)
+    private String address;
+
+    @Column(name = "phone_number", nullable = false, length = 50)
+    private String phoneNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
