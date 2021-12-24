21f38e79cea93402ef65b41f480818d1ab365d2f
Alibek <ali.mazhikenov@gmail.com>
2021-12-25 02:04:59 +0600

    Created new repository implementations

--- src/main/java/com/javaeefinal/itse1908r/javaeefinal/RepositoryImpl/GroupRepositoryImpl.java
+++ src/main/java/com/javaeefinal/itse1908r/javaeefinal/RepositoryImpl/GroupRepositoryImpl.java
@@ -1,4 +1,59 @@
 package com.javaeefinal.itse1908r.javaeefinal.RepositoryImpl;
 
-public class GroupRepositoryImpl implements {
+import com.javaeefinal.itse1908r.javaeefinal.EntityManager;
+import com.javaeefinal.itse1908r.javaeefinal.Models.Category;
+import com.javaeefinal.itse1908r.javaeefinal.Models.Group;
+import com.javaeefinal.itse1908r.javaeefinal.Repositories.GroupRepository;
+
+import javax.inject.Inject;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+import java.util.List;
+
+public class GroupRepositoryImpl implements GroupRepository {
+    @Inject
+    EntityManager em;
+
+    @Override
+    public List<Group> findAll() {
+        javax.persistence.EntityManager entityManager = em.manager();
+        try {
+            Query query = entityManager.createQuery("SELECT g FROM Group g");
+            List<Group> result = query.getResultList();
+            return result;
+        } catch (NoResultException e){
+            return null;
+        }
+    }
+
+    @Override
+    public Group findById(int id) {
+        javax.persistence.EntityManager entityManager = em.manager();
+        try {
+            Group result = entityManager.find(Group.class, id);
+            return result;
+        }
+        return null;
+    }
+
+    @Override
+    public Group updateNameById(int id, String name) {
+        javax.persistence.EntityManager entityManager = em.manager();
+        try {
+            Group result = entityManager.find(Group.class, id);
+            result.setName(name);
+            entityManager.persist(result);
+            return result;
+        }
+        return null;
+    }
+
+    @Override
+    public Group insertGroup(String name) {
+        javax.persistence.EntityManager entityManager = em.manager();
+        Group group = new Group();
+        group.setName(name);
+        entityManager.persist(group);
+        return null;
+    }
 }
---
+++ src/main/java/com/javaeefinal/itse1908r/javaeefinal/RepositoryImpl/StudentDetailImpl.java
@@ -0,0 +1,23 @@
+package com.javaeefinal.itse1908r.javaeefinal.RepositoryImpl;
+
+import com.javaeefinal.itse1908r.javaeefinal.Models.StudentDetail;
+import com.javaeefinal.itse1908r.javaeefinal.Repositories.StudentDetailRepository;
+
+import java.util.List;
+
+public class StudentDetailImpl implements StudentDetailRepository {
+    @Override
+    public List<StudentDetail> findAll() {
+        return null;
+    }
+
+    @Override
+    public StudentDetail findById(int id) {
+        return null;
+    }
+
+    @Override
+    public StudentDetail updateGroupByIdandGroupName(int id, String group) {
+        return null;
+    }
+}
